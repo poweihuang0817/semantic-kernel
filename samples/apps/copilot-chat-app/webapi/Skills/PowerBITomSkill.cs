425af7f779d0ba7c827accba5bfc881786ae4384
Po-Wei Huang <pohuan@microsoft.com>
2023-05-15 17:45:33 -0700

    Fix problems.

--- samples/apps/copilot-chat-app/webapi/Skills/PowerBITomSkill.cs
+++ samples/apps/copilot-chat-app/webapi/Skills/PowerBITomSkill.cs
@@ -25,6 +25,16 @@ public class PowerBITomSkill
         /// Table name
         /// </summary>
         public const string TableName = "tableName";
+
+        /// <summary>
+        /// Column name
+        /// </summary>
+        public const string ColumnName = "columnName";
+
+        /// <summary>
+        /// Column Type name
+        /// </summary>
+        public const string ColumnTypeName = "columnTypeName";
     }
 
     [SKFunction("Return dataset name of my power bi workspace")]
@@ -180,5 +190,62 @@ public class PowerBITomSkill
         }
 
         return promptTemplate;
+    }
+
+    [SKFunction("Given workspace name, dataset name and table name, alter a column to target type.")]
+    [SKFunctionContextParameter(Name = TomSkillParameters.WorkspaceName, Description = "Workspace name for power bi workspace")]
+    [SKFunctionContextParameter(Name = TomSkillParameters.DatasetName, Description = "Dataset name for power bi workspace")]
+    [SKFunctionContextParameter(Name = TomSkillParameters.TableName, Description = "Table name")]
+    [SKFunctionContextParameter(Name = TomSkillParameters.ColumnName, Description = "Column name")]
+    [SKFunctionContextParameter(Name = TomSkillParameters.ColumnTypeName, Description = "Target type to alter")]
+    public string AlterColumnType(SKContext context)
+    {
+        if (!context.Variables.Get(TomSkillParameters.DatasetName, out string datasetName))
+        {
+            context.Fail($"Missing variable {TomSkillParameters.DatasetName}.");
+            return $"Input insufficient. No {TomSkillParameters.DatasetName}.";
+        }
+
+        if (!context.Variables.Get(TomSkillParameters.WorkspaceName, out string workspaceName))
+        {
+            context.Fail($"Missing variable {TomSkillParameters.WorkspaceName}.");
+            return $"Input insufficient. No {TomSkillParameters.WorkspaceName}.";
+        }
+
+        if (!context.Variables.Get(TomSkillParameters.TableName, out string tableName))
+        {
+            context.Fail($"Missing variable {TomSkillParameters.TableName}.");
+            return $"Input insufficient. No {TomSkillParameters.TableName}.";
+        }
+
+        if (!context.Variables.Get(TomSkillParameters.ColumnName, out string columnName))
+        {
+            context.Fail($"Missing variable {TomSkillParameters.ColumnName}.");
+            return $"Input insufficient. No {TomSkillParameters.ColumnName}.";
+        }
+
+        if (!context.Variables.Get(TomSkillParameters.ColumnTypeName, out string ColumnTypeName))
+        {
+            context.Fail($"Missing variable {TomSkillParameters.ColumnTypeName}.");
+            return $"Input insufficient. No {TomSkillParameters.ColumnTypeName}.";
+        }
+
+        string workspaceConnection = $"powerbi://api.powerbi.com/v1.0/myorg/{workspaceName}";
+        string connectString = $"DataSource={workspaceConnection};";
+
+        using (Server server = new Server())
+        {
+            server.Connect(connectString);
+
+            string targetDatabaseName = datasetName;
+            Database database = server.Databases.GetByName(targetDatabaseName);
+            Table table = database.Model.Tables.Find(tableName);
+            Column column = table.Columns[columnName];
+            column.DataType = DataType.Decimal;
+
+            database.Model.SaveChanges();
+        }
+
+        return "Column type altered to decimal successfully.";
     }
 }
